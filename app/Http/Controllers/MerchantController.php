0158d70d7a8d5de5f452e29ca5073f1bd1afd4ee
dindazahrars <99201657+dindazahrars@users.noreply.github.com>
2022-02-09 23:01:36 +0700

    first

---
+++ app/Http/Controllers/MerchantController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Merchant;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class MerchantController extends Controller
+{
+    public function index() {
+        $data = Merchant::all();
+
+        return response($data, 200);
+    }
+
+    public function store(Request $request) {
+
+        $merchant = new Merchant();
+
+        $merchant->name = $request->name;
+        $merchant->email = $request->email;
+        $merchant->password = Hash::make($request->password);
+        $merchant->photo_profile = $request->photo_profile ?? 'user.jpg';
+        $merchant->status = $request->status;
+
+        $merchant->save();
+
+        return response($merchant, 200);
+    }
+
+    public function update(Request $request, $id) {
+
+        $merchant = Merchant::find($id);
+
+        $merchant->name = $request->name;
+        $merchant->email = $request->email;
+        $merchant->password = Hash::make($request->password);
+        $merchant->photo_profile = $request->photo_profile ?? 'user.jpg';
+        $merchant->status = $request->status;
+
+        $merchant->save();
+
+        return response($merchant, 200);
+    }
+
+    public function show ($id) {
+
+        $merchant = Merchant::findOrFail($id);
+
+        return response($merchant, 200);
+    }
+
+    public function destroy($id) {
+
+        $merchant = Merchant::findOrFail($id);
+
+        $merchant->delete();
+
+        return response('Data Berhasil Dihapus!', 200);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MerchantController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::get('/merchant', [MerchantController::class, 'index']);
+Route::post('/merchant', [MerchantController::class, 'store']);
+Route::put('/merchant/{id}/update', [MerchantController::class, 'update']);
+Route::put('/merchant/{id}', [MerchantController::class, 'show']);
+Route::delete('/merchant/{id}', [MerchantController::class, 'destroye']);
